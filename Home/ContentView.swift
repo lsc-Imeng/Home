40a1be46264b71b34d917229e0a0d1523b8c2b36
Irving Meng <imeng@lcs.on.ca>
2024-01-12 09:20:49 -0500

    2

--- Home/ContentView.swift
+++ Home/ContentView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    // MARK: Stored properties
+    let firstGradientColors = Gradient(colors: [.white, .blue])
+    // MARK: Computed properties
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                 ZStack {
+            // Background
+                     Color.orange
+                         .ignoresSafeArea()
+                     // first gradient
+                     
+                     RadialGradient(
+                        gradient: firstGradientColors,
+                        center: .center,
+                        startRadius: 180,
+                        endRadius: 300
+                     )
+
+            // Foreground
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+
         }
-        .padding()
+
     }
+
 }
 
 #Preview {
