45483e575b33cd738d9f20cbd495120053f6d9bc
Irving Meng <imeng@lcs.on.ca>
2024-01-12 08:38:35 -0500

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by 孟元森 on 2024-01-12.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
